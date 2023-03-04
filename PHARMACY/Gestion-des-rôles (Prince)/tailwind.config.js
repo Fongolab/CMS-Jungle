90e2758b5fd7f48906bf9caf9f9c0ee33c6e6a04
princelulinda <104869036+princelulinda@users.noreply.github.com>
2023-03-04 07:31:45 +0100

    Add files via upload

---
+++ PHARMACY/Gestion-des-rôles (Prince)/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/index.html"],
+  theme: {
+    extend: {
+      colors:{
+        bgblue:"#30b9bf",
+      bgGreen:"#22af91",
+      bgRed:"#ea5662",
+      bgTransparent:"#f3f3f2",
+      bgButton:"#0f70b7",
+      bgBlack:"#3a3a42",
+      color:"#efe1a8"
+      }
+    },
+  },
+  plugins: [],
+}
